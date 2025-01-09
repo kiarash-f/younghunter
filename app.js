87ceecb1bb22ff947a79bf223cb53484e7750656
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2025-01-09 19:16:18 +0330

    add user controller auth and security

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
 const express = require('express');
 const morgan = require('morgan');
+const ratelimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const galleryRouter = require('./routes/galleryRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middleware
+//1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Limit requests from same API
+const limiter = ratelimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
 
 // Routes
 app.use('/api/v1/gallery', galleryRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const galleryController = require('../controllers/galleryController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(galleryController.getAllImages)
-  .post(galleryController.createImage);
+  .post(authController.protect,galleryController.createImage);
 
 router
   .route('/:id')
   .get(galleryController.getImage)
-  .patch(galleryController.updateImage)
-  .delete(galleryController.deleteImage);
+  .patch(authController.protect,galleryController.updateImage)
+  .delete(authController.protect,galleryController.deleteImage);
 
 module.exports = router;

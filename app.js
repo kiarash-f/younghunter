a9b7f64c34809e64c987d80457c1af632e734656
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2025-01-09 16:53:31 +0330

    add global error handler and err controller

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const galleryRouter = require('./routes/galleryRoutes');
 
 const app = express();
@@ -14,4 +16,8 @@ app.use(express.json());
 // Routes
 app.use('/api/v1/gallery', galleryRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
 module.exports = app;
--- controllers/galleryController.js
+++ controllers/galleryController.js
@@ -1,94 +1,66 @@
 const Gallery = require('../models/gallerymodel');
 const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getAllImages = async (req, res) => {
-  try {
-    const features = new APIFeatures(Gallery.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const gallery = await features.query;
-    res.status(200).json({
-      status: 'success',
-      result: gallery.length,
-      data: {
-        gallery,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-exports.getImage = async (req, res) => {
-  try {
-    const image = await Gallery.findById(req.params.id);
+exports.getAllImages = catchAsync(async (req, res) => {
+  const features = new APIFeatures(Gallery.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const gallery = await features.query;
+  res.status(200).json({
+    status: 'success',
+    result: gallery.length,
+    data: {
+      gallery,
+    },
+  });
+});
+exports.getImage = catchAsync(async (req, res) => {
+  const image = await Gallery.findById(req.params.id);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        image,
-      },
-    });
-  } catch (err) {
-    console.log(req.params.id);
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
+  if (!image) {
+    return next(new AppError('No image found with that ID', 404));
   }
-};
-exports.createImage = async (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      image,
+    },
+  });
+});
+exports.createImage = catchAsync(async (req, res) => {
   //   console.log(req.body);
-  try {
-    const newImage = await Gallery.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        image: newImage,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-exports.updateImage = async (req, res) => {
-  try {
-    const image = await Gallery.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        image,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
-exports.deleteImage = async(req, res) => {
 
-  try{
-    await Gallery.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  }catch(err){
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
+  const newImage = await Gallery.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      image: newImage,
+    },
+  });
+});
+exports.updateImage = catchAsync(async (req, res) => {
+  const image = await Gallery.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!image) {
+    return next(new AppError('No image found with that ID', 404));
   }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      image,
+    },
+  });
+});
+exports.deleteImage = catchAsync(async (req, res) => {
+  await Gallery.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});

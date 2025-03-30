eecac85d9f997b33cf24e221e2aaca57268601cf
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2025-03-30 18:28:32 +0330

    tmp folder

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/image');
+    cb(null, '/tmp');
   },
   filename: (req, file, cb) => {
     console.log(file);
@@ -15,18 +15,20 @@ const upload = multer({ storage });
 const router = express.Router();
 
 router.post('/', upload.single('image'), (req, res) => {
-    console.log('Received file:', req.file); // Debugging
-    console.log('Received body:', req.body); // Debugging
-  
-    if (!req.file) {
-      return res.status(400).json({ status: 'fail', message: 'No file uploaded' });
-    }
-  
-    res.status(201).json({
-      status: 'success',
-      message: 'File uploaded successfully',
-      file: req.file,
-    });
+  console.log('Received file:', req.file); // Debugging
+  console.log('Received body:', req.body); // Debugging
+
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'No file uploaded' });
+  }
+
+  res.status(201).json({
+    status: 'success',
+    message: 'File uploaded successfully',
+    file: req.file,
   });
+});
 
 module.exports = router;

08dc50d0392316b53d70ee18f675ddc4f838c156
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2025-03-31 20:25:03 +0330

    cors

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -6,6 +6,7 @@ const APIFeatures = require('../utils/apiFeatures');
 exports.uploadFormData = catchAsync(async (req, res, next) => {
   req.body.url = `/public/image/${req.file.filename}`;
   const newImage = await Image.create(req.body);
+  console.log(newImage);
   res
     .status(201)
     .json({ message: 'File uploaded and saved!', image: newImage });

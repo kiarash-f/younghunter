94afc2ac784278789400fc3c6416ff9dd05bdac8
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2025-03-31 20:34:09 +0330

    cors 4

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -4,11 +4,11 @@ const catchAsync = require('../utils/catchAsync');
 const APIFeatures = require('../utils/apiFeatures');
 
 exports.uploadFormData = catchAsync(async (req, res, next) => {
-  // req.body.url = `/public/image/${req.file.filename}`;
-  // req.body.position = req.file.position;
-  // req.body.dateTaken = req.file.dateTaken;
+  req.body.url = `/public/image/${req.file.filename}`;
+  req.body.position = req.file.position;
+  req.body.dateTaken = req.file.dateTaken;
 
-  const newImage = await Image.create(req.body.file);
+  const newImage = await Image.create(req.body);
   console.log(newImage);
   res
     .status(201)

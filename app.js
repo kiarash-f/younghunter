c9fc133c60af10ac9279374bf35b376b3fe91049
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2025-04-02 19:22:09 +0330

    temp testing 3

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.set("Cross-Origin-Resource-Policy", "cross-origin");
   next();
 });
 
